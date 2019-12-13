df231ac9b0c95a38922ec0c7d6666cc415b06dfc
Hans Dickel <hans@recursivegeek.com>
2019-12-13 15:31:09 -0600

    Add project files.

---
+++ RecursiveGeek.RegattaBoss.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecursiveGeek.RegattaBoss.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
